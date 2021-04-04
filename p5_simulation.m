f7b3294b6db726a84c17428be0aeecaac465db06
Won Bin Choi <dc07650@naver.com>
2021-04-04 19:51:56 +0900

    Name fixing
    - Changed all the Korean descriptions to English.
    - Added a time variable regarding time limitations for the force applied on mass 1.

--- p5_simulation.m
+++ p5_simulation.m
@@ -1,34 +1,41 @@
-% dynamic simulation
+%% Engineering Vibration MATLAB
+% Author: Won Bin Choi (B3)
+% E-mail: dc07650@naver.com
+% Organization: Sogang University(Korea, Republic of), Mechanical Engineering
+% Date: April 4, 2021
 
+%% Initialize
 clear all; clc; close all;
 
 T=0.001;      % Sampling Period [sec]
-Tfinal=60;     % 시뮬레이션 시간 [sec]
-t=0:T:Tfinal; % 진행 시간 [sec]
-N=length(t);  % 시뮬레이션 Sample 갯수 [Samples]
+Tfinal=60;    % Simulation Time [sec]
+t=0:T:Tfinal; % Time Sequence [sec]
+N=length(t);  % Number of Samples [unit]
 
-L1=0.41; L2=0.54; % Manipulator 링크 길이 [m]
-m1=9; m2=1;       % Mass [kg]
-k1 = 24; k2 = 3;
+L1 = 1.5; L2 = 1.5; % Spring Free Length [m]
+m1=9;    m2=1;    % Mass [kg]
+k1 = 24; k2 = 3;  % Spring Constant [N/m]
+
 %% Generalized coordinate
-% th: 각도[rad], dth:각속도[rad/s], ddth:각가속도[rad/s^2]
-th=zeros(N,2); dth=zeros(N,2); ddth=zeros(N,2);
-tau1=zeros(N,1); tau2=zeros(N,1); tau=zeros(N,2);
-th(1,1)=0;  th(1,2)=0; %초기값 설정
+s = zeros(N,2); ds = zeros(N,2); dds = zeros(N,2);        % Displacement
+f1 = zeros(N,1); f2 = zeros(N,1); f = zeros(N,2);   % Force
+s(1,1) = 0;  s(1,2) = 0; % Initial Value
+
+Tfinal_force = Tfinal;
 
 for k=1:N
-%     토크 입력(주어진 함수)
-%     if k<2000,
-        tau1(k)=cos(1.9*t(k));
-        tau2(k)=0;
-        tau(k,:)=[tau1(k) tau2(k)];
-%     end
+    % Force Input
+    if k < round((Tfinal_force)/(Tfinal)*N,0)
+        f1(k) = cos(1.9*t(k));
+        f2(k) = 0;
+        f(k,:) = [f1(k) f2(k)];
+    end
 end
 
-%% Simulation
+%% Forward Dynamics
 for k=1:N-1
     
-    %관성력항 계산
+    % Mass Matrix
     M11(k)=m1;
     M12(k)=0;
     M21(k)=0;
@@ -36,30 +43,32 @@ for k=1:N-1
     M=[M11(k) M12(k); M21(k) M22(k)];
     M_inv =inv(M);
     
-    
+    % Spring Constant Matrix
     K11(k)=k1+k2;
     K12(k)=-k2;
     K21(k)=-k2;
     K22(k)=k2;
     K=[K11(k) K12(k); K21(k) K22(k)];
     
-    %forward dynamics (토크입력 -> 각가속도 출력)
-    C_input=tau(k,:)'-K*th(k,:)'; %+(-2*sign(dth(k,:))-1*dth(k,:));
-    ddth(k,:)=(M_inv*C_input);       %다음 샘플의 각가속도 계산
-    dth(k+1,:)=dth(k,:)+ddth(k,:)*T;  %다음샘플의 각속도 계산
-    th(k+1,:)=th(k,:)+dth(k+1,:)*T;   %다음샘플의 각도 계산
+    % Forward Dynamics (Force -> Displacement)
+    C_input=f(k,:)'-K*s(k,:)';
+    dds(k,:)=(M_inv*C_input);      % Initial acceleration definition
+    ds(k+1,:)=ds(k,:)+dds(k,:)*T;  % Linear Integration
+    s(k+1,:)=s(k,:)+ds(k+1,:)*T;   % Linear Integration
 end
 
-x=zeros(N,3); y=zeros(N,3); % 좌표계 위치 초기화
+%% Forward Kinematics
+x=zeros(N,3); y=zeros(N,3); % Initialize Coordinate Space
 for k=1:N
     x(k,1)=0;
     y(k,1)=0;
-    x(k,2)=th(k,1)+1.5;
+    x(k,2)=s(k,1) + L1;
     y(k,2)=0;
-    x(k,3)=th(k,1)+th(k,2)+1.5+1.5;
+    x(k,3)=s(k,1)+ s(k,2) + L1 + L2;
     y(k,3)=0;
 end
 
+%% Simulation
 figure('color','w');
 for k=1:200:N
     plot(x(k,1),y(k,1),'ko'); hold on; % Joint 1
@@ -73,32 +82,31 @@ for k=1:200:N
     hold off;
 end
 
-
+%% Plot
 figure('color','w');
 
 subplot(211); % Graphs of end point
-plot(t,tau1,'b','linewidth',2); hold on;
-plot(t,tau2,'r','linewidth',2); hold on;
-legend('τ_1','τ_2')
-ylabel('Torque(Nm)'); xlabel('time(sec)')
+plot(t,f1,'b','linewidth',2); hold on;
+plot(t,f2,'r','linewidth',2); hold on;
+legend('F_1','F_2')
+ylabel('Force [N]'); xlabel('Time [sec]')
 
-subplot(212); % Graphs of joint angle
-plot(t,th(:,1)*180/pi,'b','linewidth',2); hold on;
-plot(t,th(:,2)*180/pi,'r','linewidth',2); hold on;
-legend('θ_1','θ_2')
-ylabel('Angle(deg)'); xlabel('time(sec)')
-
+subplot(212); % Graphs of displacement
+plot(t,s(:,1)*180/pi,'b','linewidth',2); hold on;
+plot(t,s(:,2)*180/pi,'r','linewidth',2); hold on;
+legend('x_1','x_2')
+ylabel('Displacement [m]'); xlabel('Time [sec]')
 
 figure('color','w');
 
 subplot(311); % Graphs of end point
 plot(t,x(:,3),'b','linewidth',2); hold on;
-ylabel('x3(m)'); xlabel('time(sec)')
+ylabel('x_3 [m]'); xlabel('Time [sec]')
 
 subplot(312); % Graphs of joint angle
 plot(t,y(:,3),'r','linewidth',2); hold on;
-ylabel('y3(m)'); xlabel('time(sec)')
+ylabel('y_3 [m]'); xlabel('Time [sec]')
 
 subplot(313); % Graphs of joint angle
 plot(x(:,3),y(:,3),'b','linewidth',2); hold on;
-ylabel('y3(m)'); xlabel('x3(m)')
+ylabel('y_3 [m]'); xlabel('x_3 [m]')
